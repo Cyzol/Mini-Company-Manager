f4afbf66cc73f83a18cf0a7225f9d46f6463e311
Kuba58226 <kubaozut@gmail.com>
2020-12-21 14:05:21 +0100

    Merge

--- index.php
+++ index.php
@@ -5,11 +5,6 @@ require_once __DIR__ . '/database/FakturySprzedazy.php';
 
 session_start();
 
-$Faktura = new Invoice();
-var_dump($Faktura->select(array('id'=>1)));
-echo 'test';
-var_dump($Faktura->insert(array('invoicenumber'=>'bbb','contactordata'=>'bbb','netamount'=>1,'grossamount'=>1,'vattax'=>1,'amountincurrency'=>1,'currency'=>'bbb','url'=>'bbb')));
-
 $action = isset($_GET['action']) ? $_GET['action'] : null;
 
 switch ($action) {
