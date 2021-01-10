c3822ba6b03ac0d4a8814c11651d2903fbe3bb69
Kuba58226 <kubaozut@gmail.com>
2021-01-10 17:52:28 +0100

    Dodanie pliku InvoiceClass.php oraz utworzenie połączenia z DB dla wyświetlania faktur sprzedaży

---
+++ src/InvoiceClass.php
@@ -0,0 +1,12 @@
+<?php
+class InvoiceClass{
+    private $id;
+    private $invoiceNumber;
+    private $contactorData;
+    private $netAmount;
+    private $vatTax;
+    private $grossAmount;
+    private $amountInCurrency;
+    private $currency;
+    private $url;
+}
--- src/InvoiceRepository.php
+++ src/InvoiceRepository.php
@@ -1,8 +1,26 @@
 <?php
 require_once __DIR__ . '/../autoload.php';
+require_once __DIR__ . './../database/config.php';
 
 class InvoiceRepository
 {
+    private $connection = null;
+    private $invoicesList = [];
+
+    public function __construct(){
+        global $config;
+        $this->connection = new PDO($config['dsn'], $config['username'], $config['password']);
+        $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $this->connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        try{
+            $stmt = $this->connection->prepare('SELECT * FROM fakturysprzedazy');
+            $result = $stmt->execute();
+            print(result);
+        }catch(Exception $e){
+            throw new Exception($e->getMessage());
+        }
+    }
+
     public function getInvoice($id)
     {
         return ['abc'];
