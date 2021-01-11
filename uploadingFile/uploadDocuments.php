30309099b1333a0acad5c27e49251a7091781f85
Cyzol <karol8.9@o2.pl>
2021-01-11 12:11:16 +0100

    adding documents

---
+++ uploadingFile/uploadDocuments.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once __DIR__ . './../database/Documents.php';
+
+$target_dir = "./../uploads/Documents/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+}
+
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
+
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+} else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        $Documents = new Documents();
+        $Documents->insert(array('iddocument'=>$_POST['iddocument'],'dateinvoice'=>$_POST['dateinvoice'],'sender'=>$_POST['sender'],
+            'recipient'=>$_POST['recipient'],'notes'=>$_POST['notes']));
+        echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+}
+?>
