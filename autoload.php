31fab837b87a31e318fabf4ef443f5cbb5977750
Cyzol <karol8.9@o2.pl>
2021-01-12 14:30:37 +0100

    updating autoload.php

--- autoload.php
+++ autoload.php
@@ -24,7 +24,3 @@ require_once __DIR__ . '/templates/PurchaseInvoiceView.php';
 require_once __DIR__ . '/templates/DocumentsView.php';
 require_once __DIR__ . '/templates/EquipmentView.php';
 require_once __DIR__ . '/templates/LicenseView.php';
-require_once __DIR__ . '/src/EquipmentClass.php';
-require_once __DIR__ . '/src/EquipmentRepository.php';
-require_once __DIR__ . '/src/LicenseClass.php';
-require_once __DIR__ . '/src/LicenseRepository.php';
