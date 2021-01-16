47d1f2b716a9833689864e1005ae4a33bb6aac98
konrada111 <konrada111@gmail.com>
2021-01-16 13:37:26 +0100

    Add searchbars filters purchase invoice

--- templates/PurchaseInvoiceView.php
+++ templates/PurchaseInvoiceView.php
@@ -12,7 +12,7 @@ class PurchaseInvoiceView
         <div class="header-table">
             <h1>Purchase Invoice</h1>
         </div>
-        <?= Layout::searchbar()?>
+        <?= Layout::searchbarInvoice()?>
         <div class="table-wrapper">
             <table class="fl-table">
                 <thead>
