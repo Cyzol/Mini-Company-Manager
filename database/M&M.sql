a2b29c6caa1a03db23d6bebd6762f4eb788df3e7
Cyzol <karol8.9@o2.pl>
2021-01-18 18:13:04 +0100

    updating sql

--- database/M&M.sql
+++ database/M&M.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 18 Sty 2021, 14:09
--- Wersja serwera: 10.4.11-MariaDB
--- Wersja PHP: 7.4.6
+-- Czas generowania: 18 Sty 2021, 18:12
+-- Wersja serwera: 10.4.14-MariaDB
+-- Wersja PHP: 7.4.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -33,8 +33,17 @@ CREATE TABLE `dokumenty` (
   `DataDokumentu` date NOT NULL,
   `Nadawca` varchar(255) NOT NULL,
   `Adresat` varchar(255) NOT NULL,
-  `Notatki` varchar(10000) NOT NULL
+  `Notatki` varchar(10000) NOT NULL,
+  `URL` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Zrzut danych tabeli `dokumenty`
+--
+
+INSERT INTO `dokumenty` (`ID`, `IdentyfikatorDokumentu`, `DataDokumentu`, `Nadawca`, `Adresat`, `Notatki`, `URL`) VALUES
+(1, '123', '2021-01-03', 'Karol', 'Konrad', 'Super dokumencik', './../uploads/Documents/Dokumentacja_App_Labo_2020.pdf'),
+(2, '543', '2021-01-16', 'Kuba', 'Konrad', 'Super dokumencik2', './../uploads/Documents/Wykłady.pdf');
 
 -- --------------------------------------------------------
 
@@ -54,6 +63,16 @@ CREATE TABLE `fakturysprzedazy` (
   `Waluta` varchar(255) NOT NULL,
   `URL` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Zrzut danych tabeli `fakturysprzedazy`
+--
+
+INSERT INTO `fakturysprzedazy` (`ID`, `NumerFaktury`, `DaneKontrahenta`, `KwotaNetto`, `KwotaPodatkuVAT`, `KwotaBrutto`, `DataSprzedazy`, `KwotaNettoWWalucie`, `Waluta`, `URL`) VALUES
+(1, '4324', 'konrad kopec', 123, 232, 433, '2021-01-02', 32, 'usd', './../uploads/SalesInvoice/Dokumentacja_App_Labo_2020.pdf'),
+(2, '4343', 'Karol Cylwik', 232, 232, 433, '2021-01-02', 44, 'usd', './../uploads/SalesInvoice/Wykłady.pdf'),
+(3, '5435', 'Jakub Ogryzek', 54, 32, 332, '2021-02-19', 5432, 'zloty', './../uploads/SalesInvoice/Wykłady.pdf'),
+(4, '645645', 'Mama taty', 124, 543, 543, '2021-01-15', 54, 'zloty', './../uploads/SalesInvoice/Dokumentacja_App_Labo_2020.pdf');
 
 -- --------------------------------------------------------
 
@@ -75,6 +94,14 @@ CREATE TABLE `fakturyzakupu` (
   `URL` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Zrzut danych tabeli `fakturyzakupu`
+--
+
+INSERT INTO `fakturyzakupu` (`ID`, `IdentyfikatorFaktury`, `NumerFaktury`, `DaneKontrahenta`, `KwotaNetto`, `KwotaPodatkuVAT`, `KwotaBrutto`, `DataZakupu`, `KwotaNettoWWalucie`, `Waluta`, `URL`) VALUES
+(1, '123', '4324', 'konrad kopec', 123, 234, 433, '2021-01-12', 122, 'usd', './../uploads/PurchaseInvoice/Dokumentacja_App_Labo_2020.pdf'),
+(2, '434', '7676', 'Karol Cylwik', 124, 222, 655, '2021-01-02', 53, 'usd', './../uploads/PurchaseInvoice/Lab_12_Systemy programowych zapór sieciowych.pdf');
+
 -- --------------------------------------------------------
 
 --
@@ -94,6 +121,13 @@ CREATE TABLE `licencje` (
   `Notatki` varchar(1000) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Zrzut danych tabeli `licencje`
+--
+
+INSERT INTO `licencje` (`ID`, `NumerInwentarzowy`, `Nazwa`, `KluczSeryjny`, `DataZakupu`, `NumerFaktury`, `WsparcieTechniczneDo`, `LicencjaWaznaDo`, `NaCzyimStanie`, `Notatki`) VALUES
+(1, '432', 'Licencja Winrara', '1232-32', '2021-01-02', '1234', '2021-01-03', '2021-01-30', 'Karol Cylwik', 'Super winrar');
+
 -- --------------------------------------------------------
 
 --
@@ -112,6 +146,14 @@ CREATE TABLE `sprzet` (
   `NaCzyimStanie` varchar(255) NOT NULL,
   `Notatki` varchar(1000) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Zrzut danych tabeli `sprzet`
+--
+
+INSERT INTO `sprzet` (`ID`, `NumerInwentarzowy`, `Nazwa`, `NumerSeryjny`, `DataZakupu`, `NumerFaktury`, `GwarancjaDo`, `WartoscNetto`, `NaCzyimStanie`, `Notatki`) VALUES
+(1, '4421', 'Klawiaturka', '4343-54', '2021-01-08', '342', '2021-01-08', 123, 'Konrad', 'Super klawiaturka'),
+(2, '5454', 'Mikrofon', '432-21', '2021-01-02', '543', '2021-01-03', 522, 'Karol', 'Super majk');
 
 -- --------------------------------------------------------
 
@@ -174,31 +216,31 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT dla tabeli `dokumenty`
 --
 ALTER TABLE `dokumenty`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT dla tabeli `fakturysprzedazy`
 --
 ALTER TABLE `fakturysprzedazy`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT dla tabeli `fakturyzakupu`
 --
 ALTER TABLE `fakturyzakupu`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT dla tabeli `licencje`
 --
 ALTER TABLE `licencje`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT dla tabeli `sprzet`
 --
 ALTER TABLE `sprzet`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT dla tabeli `users`
